cb009f1e35dbb83eb2a5733fa5aade1f1a303282
saad <saadkhan8268@gmail.com>
2022-03-19 14:15:12 +0530

    applied game logic and again logic

---
+++ script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random()*20+1);
+
+let score = 20;
+
+
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value)
+    
+    if(!guess){
+        document.querySelector('.message').textContent = 'no number selected'
+    }else if(guess === secretNumber){
+        document.querySelector('.message').textContent = 'correct number'
+        document.querySelector('body').style.backgroundColor = '#60b347'
+        document.querySelector('.number').style.width = '30rem'
+        document.querySelector('.number').textContent = secretNumber
+    }else if(guess > secretNumber){
+        if(score > 1){ 
+        document.querySelector('.message').textContent = 'too high'
+        score--;
+        document.querySelector('.score').textContent = score
+        }else{
+            document.querySelector('.message').textContent = 'You lost'
+            document.querySelector('.score').textContent = 0
+        }
+    }else if(guess < secretNumber){
+        if(score > 1){ 
+        document.querySelector('.message').textContent = 'too low'
+        score--;
+        document.querySelector('.score').textContent = score
+        }else{
+            document.querySelector('.message').textContent = 'You lost'
+            document.querySelector('.score').textContent = 0
+        }
+    }
+})
+
+document.querySelector('.again').addEventListener('click', function(){
+    document.querySelector('body').style.backgroundColor = '#222'
+    document.querySelector('.message').textContent = 'Start guessing...'
+    document.querySelector('.score').textContent = '20'
+    document.querySelector('.number').textContent = '?'
+    secretNumber = Math.trunc(Math.random()*20+1);
+    document.querySelector('.guess').value = '';
+    document.querySelector('.number').style.width = '15rem'
+} )
